ddb2dbb9151320ca6ec68fcfe4558796277ff908
Jaison Bhatti <jaison.bhatti@gmail.com>
2017-10-14 13:44:36 -0400

    add tableview and add button to storyboard

--- HitList/ViewController.swift
+++ HitList/ViewController.swift
@@ -10,14 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    var names: [String] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        title = "The List"
+        tableView.register(UITableViewCell.self,
+                           forCellReuseIdentifier: "Cell")
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func addName(_ sender: UIBarButtonItem) {
+    
     }
 
 
